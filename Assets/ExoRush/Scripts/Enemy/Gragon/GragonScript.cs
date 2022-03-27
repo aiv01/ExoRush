198e3e3e1aa406b44b53df1a3fde1518d363886a
Ale3D <aleprecom@gmail.com>
2022-03-27 18:43:03 +0200

    Gragon added

---
+++ Assets/ExoRush/Scripts/Enemy/Gragon/GragonScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GragonScript : MonoBehaviour
+{
+    GameObject Ship;
+    public float EngageDistance;
+    public Animator Animator;
+    bool HasDamaged;
+    public GameObject StaticRock;
+    public GameObject Debris;
+    public bool AnimationActivated;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ship = Object.FindObjectOfType<DropShipMovement>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z - EngageDistance < Ship.transform.position.z && !AnimationActivated)
+        {
+            Animator.enabled = true;
+            StaticRock.SetActive(false);
+            Debris.SetActive(true);   
+            AnimationActivated = true;
+            
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!HasDamaged && other.name == "Dropship")
+        {
+            Object.FindObjectOfType<InGameHealth>().Damage(200, true, false, false);
+            HasDamaged = true;
+        }
+
+    }
+}
