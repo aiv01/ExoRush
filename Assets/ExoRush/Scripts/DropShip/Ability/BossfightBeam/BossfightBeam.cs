735cae1e0bb91c15b0828d255deac829c486edf4
Ale3D <aleprecom@gmail.com>
2022-03-30 20:01:32 +0200

    Better beam, overchage in progress

--- Assets/ExoRush/Scripts/DropShip/Ability/BossfightBeam/BossfightBeam.cs
+++ Assets/ExoRush/Scripts/DropShip/Ability/BossfightBeam/BossfightBeam.cs
@@ -11,6 +11,13 @@ public class BossfightBeam : MonoBehaviour
     public float StartingDamage = 1;
     public float ExponentialDamageAmplifier = 1;
 
+    //Overcharge
+    public float OverchargeSpeedAmplifier = 0.1f;
+    float Overcharge;
+    public float DischargeTime = 2;
+    float DischageTimer;
+    float DischageSpeed = 0.3f;
+
     
     
     // Start is called before the first frame update
@@ -22,34 +29,48 @@ public class BossfightBeam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(BeamStartingPoint.transform.position, BeamStartingPoint.transform.forward, out hit, Mathf.Infinity))
-        {
-            hitPos = hit.point;
-            if(hit.collider.tag == "BossSphere")
+        if (Input.GetButton("Ability")&&Overcharge < 1){
+
+            Overcharge += Time.deltaTime * OverchargeSpeedAmplifier;
+
+            Overcharge = Mathf.Clamp(Overcharge, 0, 1);
+
+            Debug.Log(Overcharge);
+
+            DischageTimer = DischargeTime;
+
+            RaycastHit hit;
+            if (Physics.Raycast(BeamStartingPoint.transform.position, BeamStartingPoint.transform.forward, out hit, Mathf.Infinity))
             {
-                Damage = Mathf.Pow(StartingDamage,HittingTime);
+                hitPos = hit.point;
+                if (hit.collider.tag == "BossSphere")
+                {
+                    Damage = Mathf.Pow(StartingDamage, HittingTime);
 
-                Debug.Log(Damage);
+                    Damage = Damage * Time.deltaTime * 1000;
 
-                Damage = Damage*Time.deltaTime * 1000;
+                    hit.collider.transform.root.GetComponent<EnemyMaster>().Damage((int)Damage);
+                    HittingTime += Time.deltaTime * ExponentialDamageAmplifier;
+                }
+                else
+                {
+                    HittingTime = 0;
+                }
 
-                hit.collider.transform.root.GetComponent<EnemyMaster>().Damage((int) Damage);
-                HittingTime += Time.deltaTime * ExponentialDamageAmplifier;
+
             }
             else
             {
                 HittingTime = 0;
             }
-            
 
+            Debug.DrawRay(BeamStartingPoint.transform.position, BeamStartingPoint.transform.forward * 9999, Color.green);
         }
         else
         {
-            HittingTime = 0;
+            DischageTimer -= Time.deltaTime * DischageSpeed;
         }
 
-        Debug.DrawRay(BeamStartingPoint.transform.position, BeamStartingPoint.transform.forward*9999, Color.green);
     }
 
     private void OnDrawGizmos()
