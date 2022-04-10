50f183c0ee48155e683bc50ef9ad0baad03ea898
Ale3D <aleprecom@gmail.com>
2022-04-11 01:13:42 +0200

    Fixes and starting transition

---
+++ Assets/ExoRush/Scripts/Transition/DefaultMapStartingWhite.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class DefaultMapStartingWhite : MonoBehaviour
+{
+    public Volume Volume;
+    public float TransitionSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Volume.weight -= Time.deltaTime * TransitionSpeed;
+        if(Volume.weight < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
