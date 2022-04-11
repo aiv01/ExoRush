5481a8927a98b22be44ff0c3b5522ac6dc2758cb
Ale3D <aleprecom@gmail.com>
2022-04-12 01:17:52 +0200

    Fixes

--- Assets/ExoRush/Scripts/DropShip/ProceduralTerrainAutomaticMovement.cs
+++ Assets/ExoRush/Scripts/DropShip/ProceduralTerrainAutomaticMovement.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class ProceduralTerrainAutomaticMovement : MonoBehaviour
 {
-    public GameObject Terrain1;
-    public GameObject Terrain2;
+    public GameObject[] TerrainPool;
     public float TerrainSize = 4097;
     public float TerrainOffset = -400;
 
@@ -18,14 +17,13 @@ public class ProceduralTerrainAutomaticMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Terrain1.transform.position.z + TerrainSize < transform.position.z)
+        //Move terrain
+        for (int i = 0; i < TerrainPool.Length; i++)
         {
-            Terrain1.transform.position = new Vector3(TerrainSize*-0.5f,TerrainOffset,Terrain1.transform.position.z + (TerrainSize * 2));
-        }
-
-        if (Terrain2.transform.position.z + TerrainSize < transform.position.z)
-        {
-            Terrain2.transform.position = new Vector3(TerrainSize * -0.5f,TerrainOffset,Terrain2.transform.position.z + (TerrainSize * 2));
+            if (TerrainPool[i].transform.position.z + TerrainSize < transform.position.z)
+            {
+                TerrainPool[i].transform.position = new Vector3(TerrainSize * -0.5f, TerrainOffset, TerrainPool[i].transform.position.z + (TerrainSize * 2));
+            }
         }
     }
 }
