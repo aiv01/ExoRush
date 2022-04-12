a1577068bc0affbd9fa1aef702f24995d1bb9a39
ImmatureOwl <matteopoli2001@gmail.com>
2022-04-12 22:33:12 +0200

    implemented LB map (EndMap)

--- Assets/ExoRush/Scripts/UI/LIVEndMap.cs
+++ Assets/ExoRush/Scripts/UI/LIVEndMap.cs
@@ -11,8 +11,6 @@ public class LIVEndMap : LIV
     private string[] lbNames = new string[10];
 
     [SerializeField] private GameObject[] lbTiles;
-    [SerializeField] private TMP_Text score;
-    [SerializeField] private TMP_Text currency;
 
     private void Awake()
     {
@@ -20,10 +18,6 @@ public class LIVEndMap : LIV
         sl.LoadFile();
         lbValues = sl.sObj.leaderboard;
         lbNames = sl.sObj.lbNames;
-        score.text = sl.sObj.score.ToString();
-        currency.text = (sl.sObj.currency + sl.sObj.score).ToString();
-        SortLB();
-        sl.UpdateObjLeaderboard(lbValues, lbNames, true);
         SetLB();
     }
 
@@ -38,24 +32,6 @@ public class LIVEndMap : LIV
                 if (value.gameObject.tag == "lbValue") value.text = lbValues[i].ToString();
                 if (value.gameObject.tag == "lbName") value.text = lbNames[i];
             }
-        }
-    }
-
-    private void SortLB()
-    {
-        //create temporary array and assign it with lbValues
-        int[] tempValues = new int[11];
-        for(int i = 0; i < tempValues.Length - 1; i++)
-        {
-            tempValues[i] = lbValues[i];
-        }
-        tempValues[10] = sl.sObj.score;
-        //sort tempArray
-        Array.Sort(tempValues);
-        //assign tempArray to lbValues. discard last item
-        for (int i = 0; i < lbValues.Length; i++)
-        {
-            lbValues[i] = tempValues[lbValues.Length - i];
         }
     }
 
