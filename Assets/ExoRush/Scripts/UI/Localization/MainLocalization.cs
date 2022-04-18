90666ed3dee9026b2c66928472cbba66dcf5171e
Ale3D <aleprecom@gmail.com>
2022-04-18 16:44:57 +0200

    More localization

--- Assets/ExoRush/Scripts/UI/Localization/MainLocalization.cs
+++ Assets/ExoRush/Scripts/UI/Localization/MainLocalization.cs
@@ -7,7 +7,6 @@ public enum Lenguage {ENG,IT,ES,JP};
 public class MainLocalization : MonoBehaviour
 {
     static public Lenguage CurrentLenguage;
-    public int LenguageInt;
 
     public delegate void InputDelegate(Lenguage type);
 
@@ -22,7 +21,6 @@ public class MainLocalization : MonoBehaviour
     {
         OnLenguageChanged(choosedlenguage);
         CurrentLenguage = choosedlenguage;
-        LenguageInt = (int)CurrentLenguage;
     }
 
 }
